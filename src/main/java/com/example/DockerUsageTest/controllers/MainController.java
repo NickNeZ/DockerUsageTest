36ab2caafd8484a5cc7f45f11f33a10b4194f111
NickNeZ <anton614000@gmail.com>
2025-09-24 14:50:04 +0500

    Small Changes

--- src/main/java/com/example/DockerUsageTest/controllers/MainController.java
+++ src/main/java/com/example/DockerUsageTest/controllers/MainController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class MainController {
@@ -12,7 +12,7 @@ public class MainController {
     private String message;
 
     @GetMapping("/")
-    public String greeting(Model model) {
+    public String Main(Model model) {
         model.addAttribute("title", "Hello World");
         model.addAttribute("message", message);
         return "index";
